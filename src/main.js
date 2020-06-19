b8a67c6f2156ef34874a418d55d60573cbb50b01
IAIIAI <dmitrenko1968@gmail.com>
2020-06-19 16:29:52 +0300

    Simple map plane added

--- src/main.js
+++ src/main.js
@@ -12,18 +12,30 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three-orbitcontrols/OrbitControls.js';
 
 /* .GLTF model loader import */
-/* import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; */
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 /* Main drawing context representation class */
 class Drawer {
   constructor (canvas) {
     this.scene = new THREE.Scene();
-    this.bgScene = new THREE.Scene();
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      './bin/skybox/pos-z.png',
+      './bin/skybox/neg-z.png',
+      './bin/skybox/pos-y.png',
+      './bin/skybox/neg-y.png',
+      './bin/skybox/pos-x.png',
+      './bin/skybox/neg-x.png'
+    ]);
+    this.scene.background = texture;
 
     this.camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 100);
-    this.camera.position.set(3, 3, 3);
+    this.camera.position.set(0, 8, 26);
 
     this.controls = new THREE.OrbitControls(this.camera, canvas);
+    this.controls.maxPolarAngle = Math.PI / 2 - 0.05;
+    this.controls.minDistance = 3;
+    this.controls.maxDistance = 47;
 
     this.renderer = new THREE.WebGLRenderer({ canvas: canvas });
     this.renderer.autoClearColor = false;
@@ -37,45 +49,24 @@ class Drawer {
 
   /* Initialize drawing context method */
   init () {
-    // Add sky wrapping background
-    /*
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('../bin/sky_bg.jpg');
-    texture.magFilter = THREE.LinearFilter;
-    texture.minFilter = THREE.LinearFilter;
-
-    const shader = THREE.ShaderLib.equirect;
-    const material = new THREE.ShaderMaterial({
-      fragmentShader: shader.fragmentShader,
-      vertexShader: shader.vertexShader,
-      uniforms: shader.uniforms,
-      depthWrite: false,
-      side: THREE.BackSide
-    });
-    material.uniforms.tEquirect.value = texture;
-    const bg = new THREE.BoxBufferGeometry(2, 2, 2);
-    this.bgMesh = new THREE.Mesh(bg, material);
-    this.bgScene.add(this.bgMesh);
-    */
-    const loader = new THREE.CubeTextureLoader();
-    const texture = loader.load([
-      './bin/pos-z.png',
-      './bin/neg-z.png',
-      './bin/pos-y.png',
-      './bin/neg-y.png',
-      './bin/pos-x.png',
-      './bin/neg-x.png'
-    ]);
-    this.scene.background = texture;
+    // Plane
+    const texture = new THREE.TextureLoader().load('./bin/map.png');
+    const plane = new THREE.Mesh(
+      new THREE.PlaneGeometry(10, 10),
+      new THREE.MeshBasicMaterial({ map: texture })
+    );
+    plane.rotateX(-Math.PI / 2);
+    this.scene.add(plane);
 
     // Test cube sample
     const test = new THREE.Mesh(
       new THREE.CubeGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({
-        color: 0x00ff00,
+        color: 0xff0000,
         wireframe: true
       })
     );
+    test.position.set(0, 0.51, 0);
     this.scene.add(test);
   }
 
