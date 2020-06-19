fa6f0cdab4002dbcb7b1b8ad5cc82ad611cef195
IAIIAI <dmitrenko1968@gmail.com>
2020-06-19 11:11:22 +0300

    Main files added; basic example built

---
+++ src/main.js
@@ -0,0 +1,80 @@
+/**********************************************
+ * School model visualization preview main file
+ **********************************************/
+
+/* CSS styles import */
+import './styles.css';
+
+/* Three.js library import */
+import * as THREE from 'three';
+
+/* Orbit controls import */
+import { OrbitControls } from 'three-orbitcontrols/OrbitControls.js';
+
+/* .GLTF model loader import */
+/* import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; */
+
+/* Main drawing context representation class */
+class Drawer {
+  constructor (canvas) {
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 100);
+    this.camera.position.set(3, 3, 3);
+
+    this.controls = new THREE.OrbitControls(this.camera, canvas);
+
+    this.renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    /* this.renderer.autoClearColor = false; */
+    this.renderer.setSize(canvas.width, canvas.height);
+    /*
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMapSoft = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    */
+  }
+
+  /* Initialize drawing context method */
+  init () {
+    const test = new THREE.Mesh(
+      new THREE.CubeGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true
+      })
+    );
+    this.scene.add(test);
+  }
+
+  /* Interframe response method */
+  response () {
+  }
+
+  /* Render method */
+  render () {
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+/* Main program drawing context */
+let drawer;
+
+/* Main render function */
+function render () {
+  window.requestAnimationFrame(render);
+
+  drawer.response();
+  drawer.render();
+}
+
+/* Start render function */
+function threejsStart () {
+  const canvas = document.getElementById('main-canvas');
+  drawer = new Drawer(canvas);
+
+  drawer.init();
+  render();
+}
+
+/* Add event handle for dynamically updating objects */
+document.addEventListener('DOMContentLoaded', threejsStart);
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'index-bundles.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(glsl|vert|frag|txt)$/,
+        use: {
+          loader: 'raw-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
