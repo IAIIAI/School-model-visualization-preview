5bb8df10988918bf6062eb2ef9cd22751e107ee2
IAIIAI <dmitrenko1968@gmail.com>
2020-06-19 13:27:29 +0300

    Skybox environment added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-/**********************************************
+/***********************************************
  * School model visualization preview main file
- **********************************************/
+ ***********************************************/
 
 /* CSS styles import */
 import './styles.css';
@@ -18,6 +18,7 @@ import { OrbitControls } from 'three-orbitcontrols/OrbitControls.js';
 class Drawer {
   constructor (canvas) {
     this.scene = new THREE.Scene();
+    this.bgScene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 100);
     this.camera.position.set(3, 3, 3);
@@ -25,7 +26,7 @@ class Drawer {
     this.controls = new THREE.OrbitControls(this.camera, canvas);
 
     this.renderer = new THREE.WebGLRenderer({ canvas: canvas });
-    /* this.renderer.autoClearColor = false; */
+    this.renderer.autoClearColor = false;
     this.renderer.setSize(canvas.width, canvas.height);
     /*
     this.renderer.shadowMap.enabled = true;
@@ -36,6 +37,38 @@ class Drawer {
 
   /* Initialize drawing context method */
   init () {
+    // Add sky wrapping background
+    /*
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('../bin/sky_bg.jpg');
+    texture.magFilter = THREE.LinearFilter;
+    texture.minFilter = THREE.LinearFilter;
+
+    const shader = THREE.ShaderLib.equirect;
+    const material = new THREE.ShaderMaterial({
+      fragmentShader: shader.fragmentShader,
+      vertexShader: shader.vertexShader,
+      uniforms: shader.uniforms,
+      depthWrite: false,
+      side: THREE.BackSide
+    });
+    material.uniforms.tEquirect.value = texture;
+    const bg = new THREE.BoxBufferGeometry(2, 2, 2);
+    this.bgMesh = new THREE.Mesh(bg, material);
+    this.bgScene.add(this.bgMesh);
+    */
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      './bin/pos-z.png',
+      './bin/neg-z.png',
+      './bin/pos-y.png',
+      './bin/neg-y.png',
+      './bin/pos-x.png',
+      './bin/neg-x.png'
+    ]);
+    this.scene.background = texture;
+
+    // Test cube sample
     const test = new THREE.Mesh(
       new THREE.CubeGeometry(1, 1, 1),
       new THREE.MeshBasicMaterial({
